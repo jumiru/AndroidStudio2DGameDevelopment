09cf29f1b6368ec82af9f5482e4dea760989f580
jumiru <juxe_m@arcor.de>
2022-10-15 09:21:33 +0200

    game loop

---
+++ app/src/main/java/com/example/androidstudio2dgamedevelopment/GameLoop.java
@@ -0,0 +1,106 @@
+package com.example.androidstudio2dgamedevelopment;
+
+import android.graphics.Canvas;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+
+public class GameLoop extends Thread {
+    private static final double MAX_UPS = 30.0;
+    private static final double UPS_PERIOD = 1E3/MAX_UPS;
+    private final Game game;
+    private boolean isRunning;
+    private SurfaceHolder surfaceHolder;
+    private double averageUPS;
+    private double averageFPS;
+
+    public GameLoop(Game game, SurfaceHolder surfaceHolder) {
+        this.surfaceHolder = surfaceHolder;
+        this.game = game;
+    }
+
+    public double getAverageUPS() {
+        return averageUPS;
+    }
+
+    public double getAverageFPS() {
+        return averageFPS;
+    }
+
+    public void startLoop() {
+        isRunning = true;
+        start();
+    }
+
+    @Override
+    public void run() {
+        super.run();
+
+        //Declare time and cycle counter variables
+        int updateCount = 0;
+        int frameCount = 0;
+
+        long startTime = 0;
+        long elapsedTime = 0;
+        long sleepTime = 0;
+
+        //Game loop
+        Canvas canvas = null;
+
+        startTime = System.currentTimeMillis();
+        while (isRunning) {
+
+            // try to update and render game
+            try {
+                canvas = surfaceHolder.lockCanvas();
+                synchronized (surfaceHolder) {
+                    game.update();
+                    updateCount++;
+
+                    game.draw(canvas);
+                }
+            } catch ( IllegalArgumentException e) {
+                e.printStackTrace();
+            } finally {
+                if ( canvas != null ) {
+                    try {
+                        surfaceHolder.unlockCanvasAndPost(canvas);
+                        frameCount++;
+                    } catch(Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            // pause game loop to not exceed target UPS
+            elapsedTime = System.currentTimeMillis() - startTime;
+            sleepTime = (long) (updateCount*UPS_PERIOD - elapsedTime );
+
+            if ( sleepTime>0 ) {
+                try {
+                    sleep(sleepTime);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            // skip frames to keep up with target UPS
+            while (sleepTime<0 && updateCount < MAX_UPS-1) {
+                game.update();
+                updateCount++;
+                elapsedTime = System.currentTimeMillis() - startTime;
+                sleepTime = (long) (updateCount*UPS_PERIOD - elapsedTime );
+            }
+
+            // Calculate average FPS and UPS
+
+            elapsedTime = System.currentTimeMillis() - startTime;
+            if ( elapsedTime > 1000 ) {
+                averageUPS = updateCount / (1E-3 * elapsedTime);
+                averageFPS = frameCount / (1E-3 * elapsedTime);
+                updateCount = 0;
+                frameCount = 0;
+                startTime = System.currentTimeMillis();
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/androidstudio2dgamedevelopment/MainActivity.java
+++ app/src/main/java/com/example/androidstudio2dgamedevelopment/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.androidstudio2dgamedevelopment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
+/**
+ * MainActivity is the main entry point to my game
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // set window to full screen
+        Window window=getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        // set content view to game so that objects of the game can be rendered to the screen
+        setContentView(new Game(this));
     }
 }
